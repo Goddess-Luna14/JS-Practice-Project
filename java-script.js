95b3ef90c5e7537bb4dd959426a85fc1eafa0efd
pilsburryDoughBoi <52458361+pilsburryDoughBoi@users.noreply.github.com>
2019-07-15 10:24:12 -0400

    Update java-script.js

--- java-script.js
+++ java-script.js
@@ -9,7 +9,7 @@ if
      var dropdowns =
 document.getElementsByClassName("dropdown-content");
      var i;
-     for (i = 0; i < dropdowns.length; i++) {
+     for (i = 0; i  dropdowns.length; i++) {
        var openDropdown =
 dropdowns[i];
        if
